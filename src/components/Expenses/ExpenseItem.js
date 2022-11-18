172ed49d4733723cc691f4bba327ae009863a502
TAMTA JOJUA <jojua.tamta99@gmail.com>
2022-11-18 23:32:07 +0400

    implemented to way binding

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("jdjdj");
+  // const [title, setTitle] = useState(props.title);
+  // console.log("jdjdj");
 
   // let title = props.title;
 
-  const clickHandler = () => {
-    setTitle("Updated");
-    // title = "Updated!";
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated");
+  //   // title = "Updated!";
+  //   console.log(title);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change title</button> */}
     </Card>
   );
 };
